d98fc3ba268129366d1e00e68a56e1b2a7446a81
Ulices Meléndez Acosta <ulices009@hotmail.com>
2021-01-30 23:31:59 -0500

    Add tests

---
+++ Test/Services/SearchFightServiceTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SearchFight.HttpClients.Interfaces;
+using SearchFight.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Services
+{
+    [TestClass]
+    public class SearchFightServiceTests
+    {
+        [TestMethod]
+        public async Task SearchFight()
+        {
+            string cSharp = "C#";
+            int cSharpGoogleTotal = 20000;
+            int cSharpBingTotal = 50000;
+
+            string js = "JavaScript";
+            int jsGoogleTotal = 30000;
+            int jsBingTotal = 40000;
+
+            var programmingLanguages = new string[]
+            {
+                cSharp,
+                js
+            };
+
+            var searchHttpClient = new Mock<ISearchHttpClient>();
+            searchHttpClient
+                .Setup(x => x.GetGoogleResults(cSharp))
+                .Returns(Task.FromResult(cSharpGoogleTotal));
+            searchHttpClient
+                .Setup(x => x.GetBingResults(cSharp))
+                .Returns(Task.FromResult(cSharpBingTotal));
+
+            searchHttpClient
+               .Setup(x => x.GetGoogleResults(js))
+               .Returns(Task.FromResult(jsGoogleTotal));
+            searchHttpClient
+                .Setup(x => x.GetBingResults(js))
+                .Returns(Task.FromResult(jsBingTotal));
+
+            var searchService = new SearchService(searchHttpClient.Object);
+            var searchFightService = new SearchFightService(searchService);
+
+            var result = await searchFightService.SearchFight(programmingLanguages);
+            var cSharpResult = result.ProgrammingLanguageResults[0];
+            var jsResult = result.ProgrammingLanguageResults[1];
+
+            Assert.AreEqual(cSharp, cSharpResult.ProgrammingLanguage);
+            Assert.AreEqual(cSharpGoogleTotal, cSharpResult.GoogleTotal);
+            Assert.AreEqual(cSharpBingTotal, cSharpResult.BingTotal);
+
+            Assert.AreEqual(js, jsResult.ProgrammingLanguage);
+            Assert.AreEqual(jsGoogleTotal, jsResult.GoogleTotal);
+            Assert.AreEqual(jsBingTotal, jsResult.BingTotal);
+
+            Assert.AreEqual(js, result.GoogleWinner);
+            Assert.AreEqual(cSharp, result.BingWinner);
+        }
+    }
+}
---
+++ Test/Services/SearchServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SearchFight.HttpClients.Interfaces;
+using SearchFight.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test.Services
+{
+    [TestClass]
+    public class SearchServiceTests
+    {
+        [TestMethod]
+        public async Task SearchProgrammingLanguageResults()
+        {
+            string programmingLanguage = "C#";
+            int googleTotal = 9878987;
+            int bingTotal = 3434234;
+
+            var searchHttpClient = new Mock<ISearchHttpClient>();
+            searchHttpClient
+                .Setup(x => x.GetGoogleResults(programmingLanguage))
+                .Returns(Task.FromResult(googleTotal));
+            searchHttpClient
+                .Setup(x => x.GetBingResults(programmingLanguage))
+                .Returns(Task.FromResult(bingTotal));
+
+            var searchService = new SearchService(searchHttpClient.Object);
+            var result = await searchService.SearchProgrammingLanguageResults(programmingLanguage);
+
+            Assert.AreEqual(programmingLanguage, result.ProgrammingLanguage);
+            Assert.AreEqual(googleTotal, result.GoogleTotal);
+            Assert.AreEqual(bingTotal, result.BingTotal);
+        }
+    }
+}
